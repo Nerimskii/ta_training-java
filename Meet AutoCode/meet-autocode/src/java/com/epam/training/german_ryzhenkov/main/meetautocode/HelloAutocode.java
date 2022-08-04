f0682f076d5599db1d6a063f331f037bec9dacf2
German Ryzhenkov <geha112233@gmail.com>
2022-08-04 18:06:19 +0400

    Add all java tasks

--- Meet AutoCode/meet-autocode/src/java/com/epam/training/german_ryzhenkov/main/meetautocode/HelloAutocode.java
+++
@@ -1,7 +0,0 @@
-package com.epam.training.german_ryzhenkov.main.meetautocode;
-
-public class HelloAutocode {
-    public static void main(String[] args) {
-        System.out.println("Hello, Autocode!");
-    }
-}
